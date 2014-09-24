6948d92d692a96b36300c3306ea28ab83c88fb81
Frank Laub <frank.laub@gmail.com>
2014-09-23 21:43:53 -0700

    Adding AGPL license

--- nbd/nbd.c
+++ nbd/nbd.c
@@ -1,10 +1,19 @@
-//
-//  nbd.c
-//  nbd
-//
-//  Created by Frank Laub on 9/13/14.
-//  Copyright (c) 2014 JFInc. All rights reserved.
-//
+/*  osx-nbd
+ *  Copyright (C) 2014 Frank Laub
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "nbd_api.h"
 
@@ -197,10 +206,10 @@ kern_return_t nbd_stop(kmod_info_t* ki, void* d)
 
     for (int i = 0; i < NBD_DEVICES; i++) {
 		printf("nbd_stop> devfs_remove(%d)\n", i);
-    	devfs_remove(nbd_table[i].bdev);
-    	nbd_table[i].bdev = NULL;
-    	nbd_table[i].dev  = 0;
-    	nbd_table[i].pid  = -1;
+		devfs_remove(nbd_table[i].bdev);
+		nbd_table[i].bdev = NULL;
+		nbd_table[i].dev  = 0;
+		nbd_table[i].pid  = -1;
     }
 
 	printf("nbd_stop> bdevsw_remove(%d)\n", nbd_bdev_major);
--- nbd/nbd_api.h
+++ nbd/nbd_api.h
@@ -1,10 +1,19 @@
-//
-//  nbd_api.h
-//  nbd
-//
-//  Created by Frank Laub on 9/13/14.
-//  Copyright (c) 2014 JFInc. All rights reserved.
-//
+/*  osx-nbd
+ *  Copyright (C) 2014 Frank Laub
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
